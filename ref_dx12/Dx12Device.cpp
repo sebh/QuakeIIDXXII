df19fdcc781f4ebc22a1eba74f99e7ea57325555
SebH <hillaire.sebastien@gmail.com>
2021-07-20 22:24:28 +0200

    Added repeat samplers

--- ref_dx12/Dx12Device.cpp
+++ ref_dx12/Dx12Device.cpp
@@ -1783,7 +1783,7 @@ RootSignature::RootSignature(RootSignatureType InRootSignatureType)
 		sampler.BorderColor = D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
 		sampler.MinLOD = 0.0f;
 		sampler.MaxLOD = D3D12_FLOAT32_MAX;
-		sampler.ShaderRegister = 0;
+		sampler.ShaderRegister = rootSamplers.size();
 		sampler.RegisterSpace = RegisterSpace;
 		sampler.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
 		rootSamplers.push_back(sampler);
@@ -1800,7 +1800,41 @@ RootSignature::RootSignature(RootSignatureType InRootSignatureType)
 		sampler.BorderColor = D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
 		sampler.MinLOD = 0.0f;
 		sampler.MaxLOD = D3D12_FLOAT32_MAX;
-		sampler.ShaderRegister = 1;
+		sampler.ShaderRegister = rootSamplers.size();
+		sampler.RegisterSpace = RegisterSpace;
+		sampler.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+		rootSamplers.push_back(sampler);
+	}
+	{
+		D3D12_STATIC_SAMPLER_DESC sampler = {};
+		sampler.Filter = D3D12_FILTER_MIN_MAG_MIP_POINT;
+		sampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+		sampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+		sampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+		sampler.MipLODBias = 0;
+		sampler.MaxAnisotropy = 0;
+		sampler.ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
+		sampler.BorderColor = D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
+		sampler.MinLOD = 0.0f;
+		sampler.MaxLOD = D3D12_FLOAT32_MAX;
+		sampler.ShaderRegister = rootSamplers.size();
+		sampler.RegisterSpace = RegisterSpace;
+		sampler.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+		rootSamplers.push_back(sampler);
+	}
+	{
+		D3D12_STATIC_SAMPLER_DESC sampler = {};
+		sampler.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+		sampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+		sampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+		sampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+		sampler.MipLODBias = 0;
+		sampler.MaxAnisotropy = 0;
+		sampler.ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
+		sampler.BorderColor = D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
+		sampler.MinLOD = 0.0f;
+		sampler.MaxLOD = D3D12_FLOAT32_MAX;
+		sampler.ShaderRegister = rootSamplers.size();
 		sampler.RegisterSpace = RegisterSpace;
 		sampler.ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
 		rootSamplers.push_back(sampler);
